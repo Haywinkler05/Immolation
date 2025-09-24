0907a81eba745e6a8b9f3ef8f02ded34fa4d6fe3
Hayden Winkler <99040410+Haywinkler05@users.noreply.github.com>
2025-09-23 20:42:15 -0500

    Added in depth comments to my input manager and player motor scripts

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,29 +3,29 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private PlayerInput.PlayerActions playerActions;
+    private PlayerInput playerInput; //This intializes our input map that is currently inside the inputs folder in assets
+    private PlayerInput.PlayerActions playerActions; //This one specifically intializes the player actions inside the input map
    
 
-    private PlayerMotor motor;
-    private void Awake()
+    private PlayerMotor motor; //Make an instance of the player motor class
+    private void Awake() //Awake happens at the start of unity
     {
-        motor = GetComponent<PlayerMotor>();
-        playerInput = new PlayerInput();
-        playerActions = playerInput.Player;
+        motor = GetComponent<PlayerMotor>(); //On awake it intializes the motor
+        playerInput = new PlayerInput(); //Creates a new instance of the player input script that is generated automatically by unity
+        playerActions = playerInput.Player; //Assins the new instance to the player map specifically in the input map
     }
-    private void FixedUpdate()
+    private void FixedUpdate() //Happens every couple of frames, normally used for physics and any movement
     {
-       motor.processMove(playerActions.Movement.ReadValue<Vector2>());
+       motor.processMove(playerActions.Movement.ReadValue<Vector2>()); //Reads the values from the movement and look map
        motor.processLook(playerActions.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
     {
-        playerActions.Enable();
+        playerActions.Enable(); 
     }
     private void OnDisable()
     {
-        playerActions.Disable();
+        playerActions.Disable(); 
     }
 }
--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
 
     private CharacterController controller; //Calls the character controller
     [Header("Player movement settings")]
@@ -13,7 +13,7 @@ public class PlayerMotor : MonoBehaviour
     public float VerticalSense = 10f;
     public float HorizontalSense = 10f;
     [SerializeField] private Camera cam;
-    void Start()
+    void Start() // Start is called once before the first execution of Update after the MonoBehaviour is created
     {
         controller = GetComponent<CharacterController>(); //Assigns the character controller
     }
@@ -25,24 +25,27 @@ public class PlayerMotor : MonoBehaviour
     }
     public void processMove(Vector2 input) //Moves the player based on the input
     {
-        Vector3 moveDirection = Vector3.zero;
+        Vector3 moveDirection = Vector3.zero; //assigns the move direction to a vector of <0,0,0>
         moveDirection.x = input.x;
-        moveDirection.z = input.y;
+        moveDirection.z = input.y;//Gets the input vectors assigned by the parameter
         controller.Move(transform.TransformDirection(moveDirection * speed * Time.deltaTime));
+        //Tells the character controller to move by transforming the player position with the speed and adjusted for frames
     }
 
 
     public void processLook(Vector2 input)
     {
-        float mouseX = input.x;
+        float mouseX = input.x; //Gets the mouse x and y location
         float mouseY = input.y;
+        
+        //The code below is for the vertical movement and will rotate around the X-axis (up and down)
+        xRotation -= mouseY * VerticalSense * Time.deltaTime; //Subtracts the roation of the x by the mouse y position multiplied by the y sense
+        xRotation = Mathf.Clamp(xRotation, -80, 80f); //This restricts the x rotation between -80 and 80
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0); //Converts the vectors into angles and will rotate the camera accordingly
 
-        xRotation -= mouseY * VerticalSense * Time.deltaTime;
-        xRotation = Mathf.Clamp(xRotation, -80, 80f);
-
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-
-        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * HorizontalSense);
+        //The code below is for  the x axis and will physically turn the player when the mouse looks left or right
+            //Why we use transform.rotate here and not cam.transform like above
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * HorizontalSense); //Allows the player to look right and left
     }
   
 }
