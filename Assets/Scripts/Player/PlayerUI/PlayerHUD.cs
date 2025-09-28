2697553ff8bf97063c662c249c708f01b0a93317
puckblocker <servergoalie0423@gmail.com>
2025-09-28 18:03:33 -0500

    Cross Hair + Cursor Lock
    Added in a temporary cross hair and locks the cursor during gameplay. I will change it to detect a settings menu.

--- Assets/Scripts/Player/PlayerUI/PlayerHUD.cs
+++ Assets/Scripts/Player/PlayerUI/PlayerHUD.cs
@@ -1,31 +1,52 @@
 using JetBrains.Annotations;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
     // Variables
 
-    public Slider healthBar;
+    public Slider healthBar;        // Slider component of the game object
     public Slider staminaBar;
     private PlayerBase playerBase;
 
+    // Detects for Script Enable
+    private void OnEnable()
+    {
+        // Locks Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Detects for Script Disable
+    private void OnDisable()
+    {
+        // Unlocks Cursor
+        Cursor.lockState = CursorLockMode.None;
+    }
+    // Custom Functions
     public void HealthStartBar()
     {
+        // Caps the Slider
         healthBar.maxValue = playerBase.gethealth();
+        // Sets the Slider's initial value
         healthBar.value = playerBase.gethealth();
     }
     public void StaminaStartBar()
     {
+        // Caps the Slider
         staminaBar.maxValue = playerBase.getStamina();
+        //Sets the Slider's initial value
         staminaBar.value = playerBase.getStamina();
     }
     public void SetHealthBar()
     {
+        // Sets the slider value
         healthBar.value = playerBase.gethealth();
     }
     public void SetStaminaBar()
     {
+        // Sets the slider value
         healthBar.value = playerBase.getStamina();
     }
 }
